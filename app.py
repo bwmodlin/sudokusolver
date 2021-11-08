fa2e2721336006430b7a662120741e46a40d1007
bwmodlin <b_modlin@coloradocollege.edu>
2021-11-08 10:29:03 -0700

    test route

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+@app.route("/test")
+def test():
+    return 'test!'
+
 if __name__ == '__main__':
     app.run()
+
