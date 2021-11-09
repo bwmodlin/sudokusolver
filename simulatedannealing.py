988c98c01fc6095ccac25bd5e25a6cc67e7265e3
bwmodlin <b_modlin@coloradocollege.edu>
2021-11-09 13:31:40 -0700

    simulated annealing first successful finish. Takes a while and sometimes gets stuck

--- simulatedannealing.py
+++ simulatedannealing.py
@@ -1,4 +1,7 @@
 import random
+from copy import deepcopy as dc
+import numpy as np
+import time
 
 PROBLEM = [
     [1, 0, 0, 0, 0, 6, 3, 0, 8],
@@ -13,6 +16,7 @@ PROBLEM = [
     [0, 4, 0, 0, 0, 7, 6, 0, 0],
     [5, 0, 7, 6, 0, 0, 0, 0, 3],
 ]
+#problem = [([0]* 9)*9]
 
 
 def get_cost(matrix):
@@ -21,10 +25,10 @@ def get_cost(matrix):
     column_list = []
 
     for i in range(len(matrix)):
-        rows_list.append(PROBLEM[i])
+        rows_list.append(matrix[i])
 
     for i in range(len(matrix[0])):
-        column_list.append([PROBLEM[j][i] for j in range(0, len(PROBLEM))])
+        column_list.append([matrix[j][i] for j in range(0, len(matrix))])
 
     def duplicate_count(array):
         already_used = {}
@@ -81,7 +85,7 @@ def set_initial_values(matrix):
 
 
 def get_new_matrix(matrix, initial):
-    try_matrix = matrix.copy()
+    try_matrix = dc(matrix)
 
     box_x = random.randint(0, 2)
     box_y = random.randint(0, 2)
@@ -91,4 +95,47 @@ def get_new_matrix(matrix, initial):
 
     set_initial_values(try_matrix)
     return try_matrix
+
+
+def run_annealing(matrix):
+    initial = dc(matrix)
+    set_initial_values(matrix)
+    temperature = 4.2
+
+    while True:
+        try_case = get_new_matrix(matrix, initial)
+        try_cost = get_cost(try_case)
+        curr_cost = get_cost(matrix)
+
+        delta_cost = try_cost - curr_cost
+        if delta_cost < 0:
+            print(try_cost)
+            matrix = try_case
+            if try_cost == 0:
+                return matrix
+            temperature *= 0.99
+        else:
+            chance = 1 / (1 + np.exp(delta_cost / temperature))
+            #print(f"chance: {chance} temperature: {temperature}")
+            if random.random() < chance:
+                print(try_cost)
+                matrix = try_case
+                if try_cost == 0:
+                    return matrix
+
+                temperature *= 0.99
+
+
+start_time = time.time()
+print(run_annealing(dc(PROBLEM)))
+print(time.time()-start_time)
+
+def get_std(matrix):
+    cost_list = []
+    for i in range(1000):
+        matrix_copy = dc(matrix)
+        set_initial_values(matrix_copy)
+        cost_list.append(get_cost(matrix_copy))
+
+    return np.std(cost_list)
 
