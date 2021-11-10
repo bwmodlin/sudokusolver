a27fa5478fc7b55258dfa45f97482ae5e127a343
Tyler Chang <tylerchang888@gmail.com>
2021-11-10 00:34:53 -0700

    Working implementation of backtracking algorithm

--- backtracking.py
+++ backtracking.py
@@ -4,22 +4,85 @@
 # Once number is placed, call that same function again recursively, if it turns out that there are no options
 # for the next cell, it means that the previous cell was wrong, so we revert back and start over on the next iteration
 
-# Functions to implement
+PROBLEM = [
+    [1, 0, 0, 0, 0, 6, 3, 0, 8],
+    [0, 0, 2, 3, 0, 0, 0, 9, 0],
+    [0, 0, 0, 0, 0, 0, 7, 1, 6],
 
-# Our main function
-# def solve():
-    # find the first blank spot by looping through the grid
-    # once blank spot is found, start an iteration through the 9 possible number choices
-    # call isPossible on the choice of of number, if True, put the number there
-    # recursively call solve() again
-    # after, set grid[x][y] = blank
-    # After the number iteration loop, return nothing / stop the function
+    [7, 0, 8, 9, 4, 0, 0, 0, 2],
+    [0, 0, 4, 0, 0, 0, 9, 0, 0],
+    [9, 0, 0, 0, 2, 5, 1, 0, 4],
 
-# Helper function that takes in a cell position and a potential input number
-# def isPossible(x,y,n):
-    # Checks to see if a number can be placed in the current position in the grid
+    [6, 2, 9, 0, 0, 0, 0, 0, 0],
+    [0, 4, 0, 0, 0, 7, 6, 0, 0],
+    [5, 0, 7, 6, 0, 0, 0, 0, 3],
+]
 
-    # 3 Checks:
-    # If able vertically
-    # If able horizontally
-    # If able in square
+
+def print_board(matrix):
+    rows_list = []
+    for i in range(len(matrix)):
+        rows_list.append(matrix[i])
+
+    for row in rows_list:
+        row.insert(3, '|')
+        row.insert(7, '|')
+    for i in range(len(rows_list)):
+        if i == 3:
+            print("-------------------------------------")
+        if i == 6:
+            print("-------------------------------------")
+        print(rows_list[i])
+
+
+def is_possible(board, row, col, num):
+    # If possible vertically
+    for i in range(9):
+        if (board[row][i] == num):
+            return False
+    # If possible horizontally
+    for i in range(9):
+        if board[i][col] == num:
+            return False
+    # If possible in the square
+    for i in range(0, 3):
+        for j in range(0, 3):
+            if board[((row // 3) * 3) + i][((col // 3) * 3) + j] == num:
+                return False
+    return True
+
+
+def solve_the_game(board):
+    row = None
+    col = None
+
+    for r in range(9):
+        for c in range(9):
+            if board[r][c] == 0:
+                row = r
+                col = c
+
+    # If no empty spots were found, we are done and return True:
+    if row is None and col is None:
+        return True
+
+    for num in range(1, 10):
+
+        if is_possible(board, row, col, num):
+            board[row][col] = num
+
+            # Recursively call the solve function again
+            if solve_the_game(board):
+                return True
+
+            # If returned false, then the choice was wrong, reset
+            board[row][col] = 0
+
+    # Backtrack
+    return False
+
+
+if solve_the_game(PROBLEM):
+    print_board(PROBLEM)
+else:
+    print("No Solution")
